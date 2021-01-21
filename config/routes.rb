cff054eea07119eddd3629958f276bbf3f2ea8f3
Dima_OHORODNIK <AlvaroMorte8888@gmail.com>
2021-01-21 10:01:50 +0200

    Update Path

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'posts#index', as: 'home'
   get 'about' => 'pages#about', as: 'about' #get - для обработки других страниц.
 
-  resources :posts #создаем REST- архитектуру.
+  resources :posts do #создаем REST- архитектуру. Отслеживание URL адресов в нутри других URL адресов.
+    resources :comments
+  end   
 end
