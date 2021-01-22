b03dbdff8262c5a911f0bd0d0b95a4f227c10453
Dima_OHORODNIK <AlvaroMorte8888@gmail.com>
2021-01-22 11:16:42 +0200

    Cоздаем форму для отображения коментария
    И устраняем ошибки допущеные в commets_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comments = @post.comments.create(comments_params)
+    @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
   
   
-  private def comments_params
-    params.require(:comments).permit(:user_name, :body)
+  private def comment_params
+    params.require(:comment).permit(:username, :body)
   end    
 end
