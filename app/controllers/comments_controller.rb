a9758d30088ca3ae2f7df7291b575bd6c4d275bd
Dima_OHORODNIK <AlvaroMorte8888@gmail.com>
2021-01-22 11:05:20 +0200

    Создаем метод Create в контроллере Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.create(comments_params)
+    redirect_to post_path(@post)
+  end
+  
+  
+  private def comments_params
+    params.require(:comments).permit(:user_name, :body)
+  end    
 end
